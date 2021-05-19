5c254f9b92fc5a918f81440d41aef33b0077d56c
Jonathan Truong <jjtruong90@gmail.com>
2021-05-19 11:12:34 -0700

    Adds css and some javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+async function getRandomMeal() {
+const randomMeal = fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+}
+
+async function getMealById(id){
+const meal = fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id)
+}
+
+async function getMealsBySearch(term){
+const meals = fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + term)
+}
